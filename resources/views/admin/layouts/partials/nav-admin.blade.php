f0f0a011ea988efb5258d99ac6ff6838906944fb
src_code_dev <scordill23@gmail.com>
2022-06-07 17:19:01 -0600

    Update admin nav

--- resources/views/admin/layouts/partials/nav-admin.blade.php
+++ resources/views/admin/layouts/partials/nav-admin.blade.php
@@ -30,8 +30,8 @@
           </button>
         </div>
         <div class="flex-shrink-0 flex items-center">
-          <img class="block lg:hidden h-8 w-auto" src="{{ asset('storage/images/logo-test2.svg') }}" alt="Workflow">
-          <img class="hidden lg:block h-8 w-auto" src="{{ asset('storage/images/logo-test2.svg') }}" alt="Workflow">
+          <a href="{{ route('welcome') }}"><img class="block lg:hidden h-8 w-auto" src="{{ asset('storage/images/logo-test2.svg') }}" alt="Workflow"></a>
+          <a href="{{ route('welcome') }}"><img class="hidden lg:block h-8 w-auto" src="{{ asset('storage/images/logo-test2.svg') }}" alt="Workflow"></a>
         </div>
         <div class="hidden md:ml-6 md:flex md:space-x-8">
           <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
@@ -56,9 +56,12 @@
           <a href="{{ route('admin.testimonials.index') }}" class="{{ Request::is('admin/testimonials*') ? 'border-indigo-500 border-b-2' : '' }} border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
             Testimonials
           </a>
-          <a href="{{ route('admin.users.index') }}" class="{{ Request::is('admin/users*') ? 'border-indigo-500 border-b-2' : '' }} border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
-            Users
-          </a>
+          @can('manage users')
+            <a href="{{ route('admin.users.index') }}" class="{{ Request::is('admin/users*') ? 'border-indigo-500 border-b-2' : '' }} border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+              Users
+            </a>
+          @endcan
+          
         </div>
       </div>
       <div class="flex items-center">
@@ -111,9 +114,15 @@
             x-transition:leave-end="transform opacity-0 scale-95"
             class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
               <!-- Active: "bg-gray-100", Not Active: "" -->
-              <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-              <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-              <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+              <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:text-gray-800 hover:bg-gray-100" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+              <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:text-gray-800 hover:bg-gray-100" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+              <form action="{{ route('logout') }}" class="d-none" method="post">
+            @csrf
+            <div class="block px-4 py-2 text-sm text-gray-700 hover:text-gray-800 hover:bg-gray-100 cursor-pointer">
+              <button type="submit" >Sign Out</button>
+            </div>
+            
+        </form>
             </div>
           </div>
         </div>
@@ -150,7 +159,12 @@
       <div class="mt-3 space-y-1">
         <a href="#" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6">Your Profile</a>
         <a href="#" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6">Settings</a>
-        <a href="#" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6">Sign out</a>
+        <form action="{{ route('logout') }}" class="d-none" method="post">
+            @csrf
+            <input type="hidden" name="_method" value="DELETE">
+            <button type="submit" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6">Delete</button>
+        </form>
+        {{-- <a href="#" class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6">Sign out</a> --}}
       </div>
     </div>
   </div>
