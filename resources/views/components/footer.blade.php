eb0d985a1f18640e80c2e6cec97fd74c00c94368
src_code_dev <scordill23@gmail.com>
2022-06-07 17:17:41 -0600

    Add components

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,55 @@
+<footer id="footer" class="bg-gradient-to-b from-primary-color to-primary-accent">
+    <x-container>
+        <div class="pt-3 flex justify-around items-center">
+            <div class="flex justify-center items-center">
+                <img class="hidden lg:block h-36 w-auto" src="http://powerof3_laravel.test/storage/images/logo-test2.svg" alt="PowerOf3 Logo">
+            </div>
+            <div class="flex justify-center items center">
+                <img class="hidden lg:block h-20 w-20" src="http://powerof3_laravel.test/storage/images/equal-housing.png" alt="Equal Housing Logo">
+                <img class="ml-1 hidden lg:block h-20 w-20" src="http://powerof3_laravel.test/storage/images/realtor2.jpg" alt="Realtor Logo">
+                <img class="ml-1 hidden lg:block border border-gray-200 h-20 w-36" src="http://powerof3_laravel.test/storage/images/lbar-logo.png" alt="Loveland Berthoud Association of Realtors Logo">
+                <img class="ml-1 hidden lg:block h-20 w-36" src="http://powerof3_laravel.test/storage/images/gara_logo.jpg" alt="Greeley Area Realtor Association">
+            </div>
+        </div>
+        <hr class="bg-gray-200">
+        <div>
+            <ul id="footer-nav" class="flex flex-wrap justify-center items-center mb-3 py-4 pl-0">
+                <li class="mx-2 md:mx-3 flex justify-center items-center text-gray-200 hover:text-blue-400">
+                    <a class="nav-link" href="../Powerof3_w_DB/index.php">Home</a>
+                </li>
+                <li class="mx-2 md:mx-3 flex justify-center items-center text-gray-200 hover:text-blue-400">
+                    <a class="nav-link" href="../Powerof3_w_DB/listings.php">Listings</a>
+                </li>
+                <li class="mx-2 md:mx-3 flex justify-center items-center text-gray-200 hover:text-blue-400">
+                    <a class="nav-link" href="../Powerof3_w_DB/about-team.php">Team</a>
+                </li>
+                <li class="mx-2 md:mx-3 flex justify-center items-center text-gray-200 hover:text-blue-400">
+                    <a class="nav-link" href="../Powerof3_w_DB/partners.php">Partners</a>
+                </li>
+                <li class="mx-2 md:mx-3 flex justify-center items-center text-gray-200 hover:text-blue-400">
+                    <a class="nav-link" href="../Powerof3_w_DB/contact.php">Contact</a>
+                </li>									
+                <li class="mx-2 md:mx-3 flex justify-center items-center text-gray-200 hover:text-blue-400">
+                    <a class="nav-link" href="../Powerof3_w_DB/testimonials.php">Testimonials</a>
+                </li>
+                <li class="mx-2 md:mx-3 flex justify-center items-center text-gray-200 hover:text-blue-400">
+                    <a class="nav-link" href="../Powerof3_w_DB/property_management.php">Property Management</a>
+                </li>
+                <li class="mx-2 md:mx-3 flex justify-center items-center text-gray-200 hover:text-blue-400">
+                    <a class="nav-link" href="../Powerof3_w_DB/choose_powerof3.php">Why Power of 3</a>
+                </li>
+                <li class="mx-2 md:mx-3 flex justify-center items-center text-gray-200 hover:text-blue-400">
+                    <a class="nav-link" href="../Powerof3_w_DB/evaluation.php">Home Evaluation</a>
+                </li>
+                <li class="mx-2 md:mx-3 flex justify-center items-center text-gray-200 hover:text-blue-400">
+                    <a class="nav-link" href="../Powerof3_w_DB/sign_up.php">Sign up with us</a>
+                </li>
+            </ul>
+        </div>
+    </x-container>	
+</footer>
+<section>
+	<p id="full-control" class="mb-0 ml-2 text-center">
+		<small><a tabindex="-1" href="../Powerof3_w_DB/admin/login.php">&copy;</a><?php echo date('Y'); ?><span class="shadow-sm">&nbsp;src_code_dev</span></small>
+	</p>
+</section>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,20 @@
+<header class="h-full bg-gradient-to-l from-primary-color to-transparent">
+    <div class="grid grid-cols-1 md:grid-cols-2 h-full">
+        <div class="justify-self-center self-center">
+            <img class="hidden lg:block h-48 w-auto" src="http://powerof3_laravel.test/storage/images/logo-test2.svg" alt="PowerOf3 Logo">
+            <h3 class="text-5xl">Specializing In</h3>
+            <h5 class="text-3xl">Homes, Land, Recreational Property</h5>
+        </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 grid-rows-3">
+            <div class="justify-self-center self-center row-start-1 row-end-1">
+                <div class="bg-house-image h-56 w-96 bg-norepeat bg-center bg-cover shadow"></div>
+            </div>
+            <div class="justify-self-center self-center col-span-2 row-start-2 row-end-2">
+                <div class="bg-hero-image h-56 w-96 bg-norepeat bg-center bg-cover shadow"></div>
+            </div>
+            <div class="justify-self-center self-center col-start-2 row-start-3 row-end-3">
+                <div class="bg-rec-image h-56 w-96 bg-norepeat bg-center bg-cover shadow"></div>
+            </div>
+        </div>
+    </div>
+</header>
