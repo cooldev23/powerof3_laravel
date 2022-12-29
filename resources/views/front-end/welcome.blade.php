f4d628a81507e0cda30e265bd28585804c52e8e1
src_code_dev <scordill23@gmail.com>
2022-12-28 21:53:56 -0700

    Update tailwind class

--- resources/views/front-end/welcome.blade.php
+++ resources/views/front-end/welcome.blade.php
@@ -18,7 +18,7 @@
                     <div class="flex-1 flex flex-col p-8">
                         <img class="w-32 h-32 flex-shrink-0 mx-auto rounded-full" src="{{ asset('storage/images/employee-images/' . $em->image_path) }}" alt="">
                         <h3 class="mt-6 text-gray-900 text-sm font-medium">{{ $em->fullname }}</h3>
-                        <dl class="mt-1 flex-grow flex flex-col justify-between">
+                        <dl class="mt-1 grow-1 flex flex-col justify-between">
                             <dt class="sr-only">Title</dt>
                             <dd class="text-gray-500 text-sm">{{ $em->first_name === 'Cathy' ? 'Owner/' . $em->type->title : $em->type->title }}</dd>
                         </dl>
